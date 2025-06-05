00a82d5c357c57bbf18001128e4d54b93463cf27
Eyasu <eyasugetaneh0@gmail.com>
2025-06-05 21:56:01 +0300

    feat: add main Express app entry point

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import express, { json } from 'express';
+import dotenv from "dotenv"
+import taskRouter from './routes/task.route.js';
+import { connectDB } from "./config/db.js";
+
+const app = express();
+
+dotenv.config()
+app.use(json());
+app.use(express.json());
+
+// API is running page
+app.get('/', (req, res) => {
+  res.send('<h1>API is running</h1>');
+});
+
+// Task routes
+app.use('/api/tasks', taskRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    connectDB();
+    console.log(`server started at http://localhost:${port}`);
+});
