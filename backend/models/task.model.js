ebca9cfcc65ab98628f49f68e41a57f737c004f4
Eyasu <eyasugetaneh0@gmail.com>
2025-06-05 22:01:39 +0300

    feat: add Task model schema for MongoDB

---
+++ backend/models/task.model.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+const TaskSchema = new Schema({
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false }
+},{
+  timestamps: true
+});
+
+const Task = mongoose.model('Task', TaskSchema)
+
+export default Task
